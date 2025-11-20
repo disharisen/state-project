cf5e71080765efaadf6edd29aa6338a6012ed0c3
Dishari Sen <ms.disharisen@gmail.com>
2025-11-20 14:12:34 +0530

    Basic Counter done .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,10 @@
-import { Button } from "@/components/shadcnui/button";
-import {
-	Card,
-	CardContent,
-	CardHeader,
-	CardTitle,
-} from "@/components/shadcnui/card";
-import { MinusCircleIcon, PlusCircleIcon } from "lucide-react";
+import BasicCounter from "@/components/BasicCounter";
+import { Card, CardHeader, CardTitle } from "@/components/shadcnui/card";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-	title: "Nextjs Starter Frontend",
-	description: "Production grade Next.js starter template",
+	title: "Basic | Counter State App",
+	description: "Basic Counter State App",
 };
 
 const page = () => {
@@ -23,22 +17,7 @@ const page = () => {
 					</CardTitle>
 				</CardHeader>
 
-				<CardContent className="space-y-4">
-					<div className="text-center text-xl">
-						Count: <span className="font-bold">0</span>
-					</div>
-
-					<div className="grid grid-cols-2 gap-4">
-						<Button
-							variant={"destructive"}
-							className="cursor-pointer">
-							<MinusCircleIcon /> Minus 1
-						</Button>
-						<Button className="cursor-pointer">
-							<PlusCircleIcon /> Plus 1
-						</Button>
-					</div>
-				</CardContent>
+				<BasicCounter />
 			</Card>
 		</section>
 	);
